49ad906dc979c39fa15c418cae231087697f2346
Mikhail <mikhailArzumanov@yandex.ru>
2022-01-13 21:48:45 +0300

    Nard factory was redacted

--- Nard_factory.h
+++ Nard_factory.h
@@ -1,26 +1,13 @@
 #pragma once
 #include "Narda.h"
 #include "color.h"
-class Nard_factory
-{
+class Nard_factory{
 private:
-	Narda nard_to_return;
+	Narda nard_to_return = Narda();
 public:
-	 Narda create_nard(ColorEnum estimated_nard_color){
-		 switch (estimated_nard_color)
-		 {
-		 case None:
-			 nard_to_return.setcolor(ColorEnum{ None });
-			 break;
-
-		 case White:
-			 nard_to_return.setcolor(ColorEnum{ White });
-
-		 case Black:
-			 nard_to_return.setcolor(ColorEnum{ Black});
-		 }
-		 return nard_to_return;
+	Narda create_nard(ColorEnum estimated_nard_color){
+		nard_to_return.setColor(estimated_nard_color);
+		return nard_to_return;
 	}
-
 };
 
